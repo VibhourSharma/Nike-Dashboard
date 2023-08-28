a6d667d0616f24fdce62698eb553e86e44983e8b
Vibhour Sharma <vibhour121@gmail.com>
2023-08-28 22:50:16 +0530

    added new styling and created new folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import Nav from "./components/Nav";
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import SuperQuality from "./sections/SuperQuality";
+import Subscribe from "./sections/Subscribe";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import Footer from "./sections/Footer";
 
-function App() {
-  return <h2 class="text-3xl font-bold underline">Working fine</h2>;
-}
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
 
 export default App;
